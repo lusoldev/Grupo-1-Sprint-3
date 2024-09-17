ae19c2d23b24e67c090626e425b3dc3ce3e61188
Martin Isaias Abdala <martinisaiasabdala@gmail.com>
2024-09-17 12:30:19 -0300

    simulador-prestamos

---
+++ src/app/prestamos/page.js
@@ -0,0 +1,9 @@
+import { SimularPrestamo } from '@/sections/prestamos';
+export const metadata = {
+	title: 'Pago de facturas - Banking',
+	description: 'Con Banking podés gestionar y pagar tus facturas rápido y fácil. '
+};
+
+export default function PrestamosPage() {
+	return <SimularPrestamo />;
+}
