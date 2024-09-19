745d9f7d6f7649ff50411cd11805aba6835f7a32
bautistaLopez04 <baulopez@uade.edu.ar>
2024-09-19 08:59:48 -0300

    Formulario contacto

---
+++ src/app/contacto/page.js
@@ -0,0 +1,9 @@
+import { Contacto } from '@/sections';
+
+export default function ContactoPage() {
+	return (
+		<main className="main-content" id="main-content">
+			<Contacto />
+		</main>
+	);
+}
